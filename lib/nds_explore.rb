a8ef8b2e09eaeab39e8555c3bb20cc8d34623b32
Beza Sirak <sirak.bezawit@gmail.com>
2020-02-19 03:19:34 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -12,7 +12,7 @@ end
 def print_first_directors_movie_titles
   row = 0 
   while row < directors_database[0][:movies].length do 
-    result = directors_database[:movies][row][:title] 
+    result = directors_database[0][:movies][row][:title] 
     puts "#{result}"
     row += 1
   end
